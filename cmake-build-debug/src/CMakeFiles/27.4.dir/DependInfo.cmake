95aec871eeed97399f7c370fbee3464b162a0589
PuShO4Ek <Stepan.Ulyanov.2007@mail.ru>
2021-10-24 13:27:18 +0400

    27.4.2 finish #1

--- cmake-build-debug/src/CMakeFiles/27.4.dir/DependInfo.cmake
+++ cmake-build-debug/src/CMakeFiles/27.4.dir/DependInfo.cmake
@@ -15,7 +15,7 @@ set(CMAKE_CXX_COMPILER_ID "Clang")
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../include"
-  "D:/Programming/c++/skill_boxinclude/classes"
+  "../include/class"
   )
 
 # The set of dependency files which are needed:
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,39 @@
 
 using namespace std;
 
-#include "Tree.h"
+#include "class/figures.h"
 
 int main()
 {
-    Tree tree = Tree();
-    tree.search();
+    cout << "Enter figure: ";
+    string figure; cin >> figure;
+
+    if (figure == "circle")
+    {
+        Circle circle = Circle();
+        cout << "S: " << circle.set_s() << endl;
+        vector<double> rSqrt = circle.set_rectangle();
+        cout << "Rectangle: " << rSqrt[0] << " " << rSqrt[1] << endl;
+    }
+    else if (figure == "triangle")
+    {
+        Triangle triangle = Triangle();
+        cout << "S: " << triangle.set_s() << endl;
+        vector<double> rSqrt = triangle.set_rectangle();
+        cout << "Rectangle: " << rSqrt[0] << " " << rSqrt[1] << endl;
+    }
+    else if (figure == "square")
+    {
+        Square square = Square();
+        cout << "S: " << square.set_s() << endl;
+        vector<double> rSqrt = square.set_rectangle();
+        cout << "Rectangle: " << rSqrt[0] << " " << rSqrt[1] << endl;
+    }
+    else if (figure == "rectangle")
+    {
+        Rectangle rectangle = Rectangle();
+        cout << "S: " << rectangle.set_s() << endl;
+        vector<double> rSqrt = rectangle.set_rectangle();
+        cout << "Rectangle: " << rSqrt[0] << " " << rSqrt[1] << endl;
+    }
 }
